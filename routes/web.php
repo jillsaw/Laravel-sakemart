61f277f81c3e8581d297546407faf322909b85ae
jillsaw <uiharu4510471@gmail.com>
2024-12-18 22:54:33 +0900

    portfolio-day8

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,5 +50,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::get('users/mypage/password/edit', 'edit_password')->name('mypage.edit_password');
         Route::put('users/mypage/password', 'update_password')->name('mypage.update_password');
         Route::get('users/mypage/favorite', 'favorite')->name('mypage.favorite');
+    });
+
+    //カートの中身表示
+    Route::controller(CartController::class)->group(function () {
+        Route::get('users/carts', 'index')->name('carts.index');
+        Route::post('users/carts', 'store')->name('carts.store');
+        Route::delete('users/carts', 'destroy')->name('carts.destroy');
     });
 });
